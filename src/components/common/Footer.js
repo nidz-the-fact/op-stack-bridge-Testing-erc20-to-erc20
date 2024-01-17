5c23dae0213c1551a9484a82c8c32ef3dbfd0284
HyprMax <max@hypr.network>
2024-01-16 17:40:00 -0800

    UI Cleanup

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import "../../assets/style/common/footer.scss";
 import { BsDiscord } from "react-icons/bs";
 import { AiFillTwitterCircle } from "react-icons/ai";
-import { FaFacebook } from "react-icons/fa"
+import logo from "../../assets/images/full_white_mixed.svg";
+import { FaTelegram } from "react-icons/fa"
+import { Image } from "react-bootstrap";
 const Footer = () => {
   return (
     <>
@@ -11,11 +13,12 @@ const Footer = () => {
         {/* <Container fluid> */}
         <div className='footer_text_wrap'>
           <ul>
-            <li><Link to="https://www.hypr.network">Hypr</Link></li>
+            <li><Link to="https://www.hypr.network"><Image src={logo} alt="logo" width="50" fluid /></Link></li>
           </ul>
         </div>
         <div className='footer_icn_wrap'>
           <ul>
+          <li><Link to="https://t.me/hyprnetwork"><FaTelegram /></Link></li>
             <li><Link to="https://twitter.com/hypr_network"><AiFillTwitterCircle /></Link></li>
           </ul>
         </div>
