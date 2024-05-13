257b4fcf29d69a058a7c27e8811248964f7aa955
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2024-05-13 22:02:22 +0700

    Update WithdrawAccount.js

--- src/components/account/WithdrawAccount.js
+++ src/components/account/WithdrawAccount.js
@@ -35,12 +35,12 @@ const WithdrawAccount = () => {
     const bridges = {
       Standard: {
         l1Bridge: l1Contracts.L1StandardBridge,
-        l2Bridge: process.env.REACT_APP_L2_BRIDGE,
+        l2Bridge: "0x4200000000000000000000000000000000000010",
         Adapter: optimismSDK.StandardBridgeAdapter,
       },
       ETH: {
         l1Bridge: l1Contracts.L1StandardBridge,
-        l2Bridge: process.env.REACT_APP_L2_BRIDGE,
+        l2Bridge: "0x4200000000000000000000000000000000000010",
         Adapter: optimismSDK.ETHBridgeAdapter,
       },
     };
